deeb2c15d210d85494586e601e3249c34160bd0b
Shoma Mandal <shoma.mandal@bobosync.com>
2021-03-24 14:19:11 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import {withAuthenticator} from 'aws-amplify-react-native';
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
+  },
+});
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default withAuthenticator(App);
